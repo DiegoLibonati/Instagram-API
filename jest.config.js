494dfa8bfc58d0b1f8ddabd66264cc170f1e333f
Die <dlibonati@leafnoise.io>
2025-01-01 18:18:04 -0300

    patch: update json config

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,4 @@ module.exports = {
   testEnvironment: "node",
   globalSetup: '<rootDir>/src/tests/jest.setup.ts',
   globalTeardown: '<rootDir>/src/tests/jest.teardown.ts',
-  transform: {
-    "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest",
-  },
-  transformIgnorePatterns: ["node_modules/(?!variables/.*)"],
-  testEnvironmentOptions: {
-    customExportConditions: [""],
-  },
 };
